8850b37c274467a68d8291e44b853ab24988818e
soydanzon <gtkach@hotmail.com>
2026-08-10 20:35:34 +0000

    3.22 Correcciones del login

--- app/lib/supabase/client.ts
+++ app/lib/supabase/client.ts
@@ -1,8 +1,18 @@
 import { createBrowserClient } from "@supabase/ssr";
 
+let supabase:
+  | ReturnType<typeof createBrowserClient>
+  | undefined;
+
 export function createClient() {
-  return createBrowserClient(
+  if (supabase) {
+    return supabase;
+  }
+
+  supabase = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
   );
+
+  return supabase;
 }
